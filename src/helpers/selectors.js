afd2985a26ebd85ff4453e2f61805abb5b23b004
JakeFantin <jakefantin@gmail.com>
2019-10-30 18:48:18 -0700

    added selectors that get interviewers and appointments for a given day, using a given state, along with unit tests

---
+++ src/helpers/selectors.js
@@ -0,0 +1,45 @@
+export function getAppointmentsForDay(state, day) {
+  let currentAppointments = [];
+  let currentDay = null;
+  if (day) {
+    for (let weekDay of state.days) {
+      if (weekDay.name === day) {
+        currentDay = weekDay;
+      }
+    }
+  }
+  if (currentDay) {
+    currentAppointments = currentDay.appointments.map(appointment => {
+      return state.appointments[appointment];
+    });
+  }
+
+  return currentAppointments;
+}
+
+export function getInterview(state, interview) {
+  if (interview) {
+    return { ...interview, interviewer: state.interviewers[interview.interviewer] };
+  } else {
+    return null;
+  }
+}
+
+export function getInterviewersForDay(state, day) {
+  let currentInterviewers = [];
+  let currentDay = null;
+  if (day) {
+    for (let weekDay of state.days) {
+      if (weekDay.name === day) {
+        currentDay = weekDay;
+      }
+    }
+  }
+  if (currentDay) {
+    currentInterviewers = currentDay.interviewers.map(interviewer => {
+      return state.interviewers[interviewer];
+    });
+  }
+
+  return currentInterviewers;
+}
