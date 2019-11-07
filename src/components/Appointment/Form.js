4b14d9c12d4860347ebee7cfaaf420d78a93068c
JakeFantin <jakefantin@gmail.com>
2019-11-06 18:28:47 -0800

    added data-testid to the input form for testing

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -7,6 +7,7 @@ import Button from 'components/Button';
 export default function Form(props) {
   const [name, setName] = useState(props.name || '');
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState('');
 
   const reset = function() {
     setName('');
@@ -16,7 +17,13 @@ export default function Form(props) {
     reset();
     props.onCancel();
   }
-  const save = function() {
+
+  function validate() {
+    if (name === '') {
+      setError("Student name cannot be blank");
+      return;
+    }
+    setError('');
     props.onSave(name, interviewer);
   }
 
@@ -30,17 +37,16 @@ return <main className="appointment__card appointment__card--create">
         placeholder={"Enter Student Name"}
         value={name}
         onChange={(event) => setName(event.target.value)}
-        /*
-          This must be a controlled component
-        */
+        data-testid="student-name-input"
       />
     </form>
+    <section className="appointment__validation">{error}</section>
     <InterviewerList interviewers={props.interviewers} interviewer={interviewer} setInterviewer={setInterviewer} />
   </section>
   <section className="appointment__card-right">
     <section className="appointment__actions">
       <Button danger onClick={cancel}>Cancel</Button>
-      <Button confirm onClick={save}>Save</Button>
+      <Button confirm onClick={validate}>Save</Button>
     </section>
   </section>
 </main>
